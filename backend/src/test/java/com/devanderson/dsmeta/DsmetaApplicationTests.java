5c6f7a41dc4a96e730a316427b0d26303fc45eee
Anderson Mendes <mendesanderson567@gmail.com>
2022-07-13 20:12:27 -0300

    repository created

---
+++ backend/src/test/java/com/devanderson/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devanderson.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
