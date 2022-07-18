237e8117febc5d9674620c54a1ee44425fc684cf
Anderson Mendes <mendesanderson567@gmail.com>
2022-07-17 22:25:49 -0300

    SMS Twilio

---
+++ backend/src/main/java/com/devanderson/dsmeta/services/SmsService.java
@@ -0,0 +1,53 @@
+package com.devanderson.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.devanderson.dsmeta.entities.Sale;
+import com.devanderson.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	SaleRepository saleRepository;
+	
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String month = Integer.toString(sale.getDate().getMonthValue());
+		//if (month.length() == 1) month = "0" + month; 
+		
+		String year = Integer.toString(sale.getDate().getYear());
+ 		
+		String date = month + "/" + year; 
+		
+		String msg = "O vendedor " + sale.getSellerName() + " realizou " + sale.getDeals() + " vendas em " + date + " com um total de R$ " + sale.getAmount();
+		
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
